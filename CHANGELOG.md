6a3112806d3cbcc2438706d0382bc7b3fb5e2383
merdok <merdok87@o2.pl>
2019-08-13 18:19:50 +0200

    Bump version to 0.8.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.8.1] - 2019-08-13
+### Fixed
+- Fixed a bug where optional switches could not be disabled
+
 ## [0.8.0] - 2019-08-04
 ### Initial release
 
--- index.js
+++ index.js
@@ -94,7 +94,7 @@ class xiaomiFanAccessory {
             .setCharacteristic(Characteristic.Manufacturer, 'Xiaomi')
             .setCharacteristic(Characteristic.Model, modelName)
             .setCharacteristic(Characteristic.SerialNumber, this.ip)
-            .setCharacteristic(Characteristic.FirmwareRevision, '0.8.0');
+            .setCharacteristic(Characteristic.FirmwareRevision, '0.8.1');
 
         this.enabledServices.push(this.informationService);
 
